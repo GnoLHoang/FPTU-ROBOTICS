0fc0673facc2896aa810a9aa5ef9cc6d558f0caf
polaris2853 <154751589+polaris2853@users.noreply.github.com>
2025-06-10 10:21:53 +0700

    Update ps2_control.ino
    If switching keys too fast, setMotorSpeed -2000 right after 2000 is possible, causing back-EMF and effectively frying the board. This update added slowly acceleration and deceleration to  prevent back-EMF

--- ps2_control.ino
+++ ps2_control.ino
@@ -1,18 +1,37 @@
-#include <PS2X_lib.h>
-#include <Adafruit_PWMServoDriver.h>
-#include <Wire.h>
-#include <MotorDriver.h>
-#include <Servo.h>
+#include "PS2X_lib.h"
+#include "Adafruit_PWMServoDriver.h"
+#include "Wire.h"
+#include "MotorDriver.h"
+#include "Servo.h"
 
+// Initialization
 PS2X ps2x;
 MotorDriver motor;
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 SimpleServo myServo;
 
 int servoAngle = 90;
+unsigned long pressed_time;
+unsigned long released_time;
+unsigned long lastInputTime;
+int left_speed = 0;
+int right_speed = 0;
 
 void setServo(uint8_t channel, uint16_t pulse) {
   pwm.setPWM(channel, 0, pulse);
+}
+
+void deceleration(int speed) {
+    const int step = 1000;
+
+    if (speed > 0) {
+        speed -= step;
+        if (speed < 0) speed = 0;
+    } else if (speed < 0) {
+        speed += step;
+        if (speed > 0) speed = 0;
+    }
+    return speed;
 }
 
 void setup() {
@@ -24,78 +43,58 @@ void setup() {
   pwm.setPWMFreq(50); 
   motor.setMotorSpeed(0, 0, 0, 0);
 
-  // Servo setup
-  // myServo.attach(6); // Gắn servo vào chân số 6
-  // myServo.write(servoAngle);
-
   // PS2 setup
-int error = -1; 
-for (int i = 0; i < 10; i++) // thử kết nối với tay cầm ps2 trong 10 lần 
-{
-  delay(1000); // đợi 1 giây 
-  // cài đặt chân và các chế độ: GamePad
-  error = ps2x.config_gamepad(14, 13, 15, 12, false, false); 
-  Serial.print("."); 
-  if(!error) //kiểm tra nếu tay cầm đã kết nối thành công 
-	break; // thoát khỏi vòng lặp 
-} 
+  int error = -1; 
+  for (int i = 0; i < 10; i++) // thử kết nối với tay cầm ps2 trong 10 lần 
+  {
+    delay(1000); // đợi 1 giây 
+    // cài đặt chân và các chế độ: GamePad
+    error = ps2x.config_gamepad(14, 13, 15, 12, false, false); 
+    Serial.print("."); 
+    if(!error) //kiểm tra nếu tay cầm đã kết nối thành công 
+    break; // thoát khỏi vòng lặp 
+  } 
 }
+
 void loop() {
-  int speed = 0;
+  bool anyPressed = false;
   ps2x.read_gamepad(false, false);
 
   // Đọc joystick trái (điều khiển motor tiến/lùi)
   int ly = ps2x.Analog(PSS_LY); // 0 - 255, giữa là ~128
 
-  speed = map(ly, 0, 255, 4095, -4095); // Joystick lên là tiến
-
-  // Xử lý xoay trái/phải nếu bấm nút
-  if (ps2x.Button(PSB_R1)) {
-    motor.setMotorSpeed(0, 0, 2000, -2000); // quay phải
-  } else if (ps2x.Button(PSB_L1)) {
-    motor.setMotorSpeed(0, 0, -2000, 2000); // quay trái
-  } else {
-    motor.setMotorSpeed(0,0,0,0);
+  // Movement
+  if (ps2x.Button(PSB_PAD_LEFT)) {    // LEFT
+    lastInputTime = millis();
+    anyPressed = true;
+    right_speed += 400;
+  }
+  if (ps2x.Button(PSB_PAD_RIGHT)) {   // RIGHT
+    lastInputTime = millis();
+    anyPressed = true;
+    left_speed -= 400;
+  }
+  if (ps2x.Button(PSB_PAD_UP)) {      // UP
+    lastInputTime = millis();
+    anyPressed = true;
+    left_speed -= 300;
+    right_speed += 300;
+  }
+  if (ps2x.Button(PSB_PAD_DOWN)) {    // DOWN
+    lastInputTime = millis();
+    anyPressed = true;
+    left_speed += 300;
+    right_speed -= 300;
   }
 
-  //servo
-  // Set servo to 0 before test
-  // setServo(2, 205); 
-  // Serial.print("set 0");
-  // delay(3000);
-
-  // Sweep from 0 to 4000 in steps of 100
-// for (int angle = 0; angle <= 180; angle += 10) {
-//   int pwm_val = map(angle, 0, 180, 100, 550); // ánh xạ góc về pwm
-//   Serial.print("Angle: ");
-//   Serial.print(angle);
-//   Serial.print(" => PWM: ");
-//   Serial.println(pwm_val);
-
-//   pwm.setPWM(2, 0, pwm_val);
-//   delay(300);
-// }
-
-// for (int pwm_val = 100; pwm_val <= 600; pwm_val += 10) {
-//   pwm.setPWM(2, 0, pwm_val);
-//   Serial.print("PWM: ");
-//   Serial.println(pwm_val);
-//   delay(500);
-// }
-
-
-  // Set servo to 0 after test
-  // setServo(2, 0); 
-
-  if (speed >= 0)
-  {
-    motor.setMotorSpeed(0, 0, speed, speed);
+  if (!anyPressed && (millis() - lastInputTime > 500)) { // Reduce speed to 0 when idling for more than 500ms
+    left_speed = deceleration(left_speed)
+    right_speed = deceleration(right_speed)
   }
-  else
-  {
-    motor.setMotorSpeed(0, 0, 0, 0);
-  }
-  motor.setMotorSpeed(0, 0, 0, 0);
+
+  motor.setMotorSpeed(0, 0, left_speed, right_speed); // Execute
+  //delay(50); No longer required, best to add to reduce serial speed for debugging
+  
 
   //Servo điều khiển bằng joystick phải trục X
   int rx = ps2x.Analog(PSS_RX); // 0 - 255
